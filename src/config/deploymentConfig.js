c5e0c18f92aabc9ddee7d9a7766adb60aa8d92b9
Soroush Usefi <behtarinrah711@gmail.com>
2026-08-31 23:45:52 +0330

    config: enable isolated Firebase project

--- src/config/deploymentConfig.js
+++ src/config/deploymentConfig.js
@@ -1,8 +1,15 @@
 export const DEPLOYMENT_CONFIG = Object.freeze({
   instanceId: 'project-tree-next-v1',
   storageNamespace: 'ptnext-v1',
-  cloudEnabled: false,
-  firebase: null,
+  cloudEnabled: true,
+  firebase: Object.freeze({
+    apiKey: 'AIzaSyD6rBk4nMvSQj986BN-Es85KXA7ZTKKNiQ',
+    authDomain: 'project-tree-next.firebaseapp.com',
+    projectId: 'project-tree-next',
+    storageBucket: 'project-tree-next.firebasestorage.app',
+    messagingSenderId: '97129426242',
+    appId: '1:97129426242:web:7790357c1b90b16e7becdb',
+  }),
 });
 
 export function namespacedStorageKey(name){
