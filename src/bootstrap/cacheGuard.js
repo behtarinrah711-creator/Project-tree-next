95b9f6f3192ca98100492166ffcb040b6b9dc1e5
Soroush Usefi <behtarinrah711@gmail.com>
2026-09-02 02:46:46 +0330

    chore: refresh development cache version

--- src/bootstrap/cacheGuard.js
+++ src/bootstrap/cacheGuard.js
@@ -1,5 +1,5 @@
 (function(){
-  const DEV_CACHE_VERSION = '20260902-0230';
+  const DEV_CACHE_VERSION = '20260902-0240';
   const refreshKey = `karha:dev-cache-refresh:${DEV_CACHE_VERSION}`;
 
   if('serviceWorker' in navigator){
