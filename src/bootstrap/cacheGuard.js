7d6cf67224dbae209cd18843ca66920528243434
Soroush Usefi <behtarinrah711@gmail.com>
2026-09-02 00:26:25 +0330

    chore: refresh WBS label cache

--- src/bootstrap/cacheGuard.js
+++ src/bootstrap/cacheGuard.js
@@ -1,5 +1,5 @@
 (function(){
-  const DEV_CACHE_VERSION = '20260901-2312';
+  const DEV_CACHE_VERSION = '20260902-0001';
   const refreshKey = `karha:dev-cache-refresh:${DEV_CACHE_VERSION}`;
 
   if('serviceWorker' in navigator){
