8a8c34cf183f7bd1e2f06dfd434cdfb4203178ad
Soroush Usefi <behtarinrah711@gmail.com>
2026-09-01 22:22:32 +0330

    chore: refresh dev cache for simple view checkboxes

--- src/bootstrap/cacheGuard.js
+++ src/bootstrap/cacheGuard.js
@@ -1,5 +1,5 @@
 (function(){
-  const DEV_CACHE_VERSION = '20260901-2147';
+  const DEV_CACHE_VERSION = '20260901-2220';
   const refreshKey = `karha:dev-cache-refresh:${DEV_CACHE_VERSION}`;
 
   if('serviceWorker' in navigator){
