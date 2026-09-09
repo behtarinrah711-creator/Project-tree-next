333a59f53136473824042af994a3e2393091d9d0
Soroush Usefi <behtarinrah711@gmail.com>
2026-09-10 01:36:16 +0330

    fix(wbs): preserve html quote escaping

--- src/modules/wbs/workTaskView.js
+++ src/modules/wbs/workTaskView.js
@@ -18,7 +18,7 @@ const TYPE_CLASSES = new Map([
 
 function escapeHtml(value){
   return String(value ?? '').replace(/[&<>"']/g, char => ({
-    '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;',
+    '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;',
   }[char]));
 }
 
