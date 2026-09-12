ce8a4ed77c865db5f0000a7c5c9d97efc69bb905
Soroush Usefi <behtarinrah711@gmail.com>
2026-09-12 12:53:12 +0330

    test(notebook): wait for application startup

--- tests/e2e/notebook.spec.js
+++ tests/e2e/notebook.spec.js
@@ -16,6 +16,7 @@ test.beforeEach(async ({ page }) => {
     }));
   });
   await page.goto('/');
+  await page.waitForFunction(() => Boolean(window.KarhaLegacy && window.KarhaApp));
   await page.locator('#avatarBtn').click();
   await page.locator('#globalNotebookBtn').click();
 });
